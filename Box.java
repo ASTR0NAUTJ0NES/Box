2e51c8603faa2ca26fd2809b62bbba6e942c23d0
Slick Dev <36201692+ASTR0NAUTJ0NES@users.noreply.github.com>
2018-10-08 10:47:11 -0400

    first commit

---
+++ Box.java
@@ -0,0 +1,15 @@
+/**
+ * Box - Lab 1-2.
+ * 
+ * @author Andrew P. Albanese
+ * @version 2016-01-18
+ */
+public class Box
+{
+    public static void main(String[] args)
+    {
+        System.out.println("+--------+");
+        System.out.println("| Andrew |");
+        System.out.println("+--------+");
+    }
+}
